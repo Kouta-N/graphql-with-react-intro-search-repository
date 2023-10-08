97d381890d0654977412126ee1794b3b66a93d86
KN <nk29time@gmail.com>
2023-10-08 18:03:11 +0900

    setup env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
+import React from "react";
+
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const App = () => {
   return <div>Hello, GraphQL</div>;
-}
+};
 
 export default App;
